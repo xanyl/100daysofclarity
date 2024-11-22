2038784b14913a43a462112aeed5a024012aa1cc
xanyl <aniltiwari645@gmail.com>
2024-11-22 14:18:53 +0545

    Day 64 - Automated Tests Continued I

--- tests/nft-advance.test.ts
+++ tests/nft-advance.test.ts
@@ -1,21 +1,15 @@
+import { Clarinet, Tx, Chain, Account, types,Contract } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-import { describe, expect, it } from "vitest";
 
-const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+Clarinet.test({
+  name: "Ensure that the right amount of stacks (NFT price) is transferred)",
+  async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+    let deployer = accounts.get("deployer")!;
+    let wallet_1 = accounts.get("wallet_1")!;
+    
+     
+}
+)
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
-
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
-});
+// Clarinet.test()
--- tests/nft-simple.test.ts
+++ tests/nft-simple.test.ts
@@ -1,21 +1,46 @@
+import { Clarinet, Tx, Chain, Account, types, Contract } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-import { describe, expect, it } from "vitest";
 
-const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+Clarinet.test({
+  name: "Ensure that the right amount of stacks (NFT price) is transferred)",
+  async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+    let deployer = accounts.get("deployer")!;
+    let wallet_1 = accounts.get("wallet_1")!;
+    let block = chain.mineBlock([
+      Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
+    ]);
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+    block.receipts[0].result.expectOk().expectBool(true);
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
+    console.log(block.receipts[0].result.expectOk().expectBool(true));
+    block.receipts[0].events.expectSTXTransferEvent(
+      10000000,
+      wallet_1.address,
+      deployer.address
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
-});
+    )
+  }
+})
+// Clarinet.test()
+
+Clarinet.test({
+  name: "Ensure that the right NFT is minted to the right address",
+  async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+    let deployer = accounts.get("deployer")!;
+    let wallet_1 = accounts.get("wallet_1")!;
+    let block = chain.mineBlock([
+      Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
+    ]);
+
+    block.receipts[0].result.expectOk().expectBool(true);
+
+    console.log(block.receipts[0].result.expectOk().expectBool(true));
+    block.receipts[0].events.expectNonFungibleTokenMintEvent(
+      10000000,
+      wallet_1.address,
+      deployer.address
+
+    )
+  }
+})
